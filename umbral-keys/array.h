5b2a2fc9ae1f54ef6eec9414cbddf0d87fc2016a
aldia_thinkpad <futami16237@gmail.com>
2025-04-06 23:29:12 +0800

    array的函数定义移动到class里面

--- umbral-keys/array.h
+++ umbral-keys/array.h
@@ -17,152 +17,101 @@ class Array {
   size_t size;      // 当前大小
 
   // 扩展数组容量
-  void expand();
+  void expand() {
+    if (size < capacity) {
+      return;  // 无需扩容
+    }
+
+    size_t newCapacity = (capacity == 0) ? 3 : capacity * 2;
+    T *resized = new T[newCapacity];  // 可能抛出 std::bad_alloc
+
+    // 仅当原 data 有效时才拷贝数据
+    if (data != nullptr) {
+      for (size_t i = 0; i < size; ++i) {
+        resized[i] = data[i];  // 确保 size <= capacity
+      }
+      delete[] data;
+    }
+
+    data = resized;
+    capacity = newCapacity;
+  }
 
  public:
   // 构造函数
-  Array();
+  Array() {
+    data = nullptr;
+    capacity = 0;
+    size = 0;
+  }
 
-  Array(size_t size);
+  Array(size_t size) {
+    if (size <= 0) {
+      throw invalid_argument("[UmbralKeys Array] size must be greater than 0");
+    }
+    data = new T[size];
+    capacity = size;
+    this->size = size;
+  }
 
-  Array(initializer_list<T> list);
-  ;
+  Array(initializer_list<T> list) : size(list.size()), capacity(list.size()) {
+    data = new T[capacity];
+    copy(list.begin(), list.end(), data);
+  }
 
   // 析构函数
-  ~Array();
+  ~Array() { delete[] data; }
 
   // 添加元素
-  void push(const T &value);
+  void push(const T &value) {
+    expand();
+    data[size] = value;
+    size++;
+  }
 
   // 删除指定下标的元素
-  void remove(size_t index);
+  void remove(size_t index) {
+    if (index >= size || size == 0) {
+      throw out_of_range("[UmbralKeys Array::remove] Index out of range");
+    }
+
+    for (size_t i = index; i < size - 1; ++i) {
+      data[i] = data[i + 1];
+    }
+    --size;
+  }
 
   // 清空所有元素
-  void clear();
+  void clear() { size = 0; }
 
   // 获取当前数组的长度
-  size_t getSize() const;
+  size_t getSize() const { return size; }
 
-  NativeArray<T> toNativeArray() const;
+  NativeArray<T> toNativeArray() const {
+    // 返回一个新的数组，包含当前的元素
+    T *arr = new T[size];
+    for (size_t i = 0; i < size; ++i) {
+      arr[i] = data[i];
+    }
+
+    NativeArray<T> nativeArray;
+    nativeArray.data = arr;
+    nativeArray.size = size;
+    return nativeArray;
+  }
 
   // 获取指定索引的元素
-  T &operator[](size_t index);
-
-  const T &operator[](size_t index) const;
-};
-
-// 扩展数组容量
-template <typename T>
-void Array<T>::expand() {
-  if (size < capacity) {
-    return;  // 无需扩容
-  }
-
-  size_t newCapacity = (capacity == 0) ? 3 : capacity * 2;
-  T *resized = new T[newCapacity];  // 可能抛出 std::bad_alloc
-
-  // 仅当原 data 有效时才拷贝数据
-  if (data != nullptr) {
-    for (size_t i = 0; i < size; ++i) {
-      resized[i] = data[i];  // 确保 size <= capacity
+  T &operator[](size_t index) {
+    if (index >= size || index < 0 || size == 0) {
+      throw out_of_range("[UmbralKeys Array::operator[]] Index out of range");
     }
-    delete[] data;
+    return data[index];
   }
 
-  data = resized;
-  capacity = newCapacity;
-}
-
-// 构造函数
-template <typename T>
-Array<T>::Array() {
-  data = nullptr;
-  capacity = 0;
-  size = 0;
-}
-
-template <typename T>
-Array<T>::Array(size_t size) {
-  if (size <= 0) {
-    throw invalid_argument("[UmbralKeys Array] size must be greater than 0");
+  const T &operator[](size_t index) const {
+    if (index >= size || index < 0 || size == 0) {
+      throw out_of_range("[UmbralKeys Array::operator[]] Index out of range");
+    }
+    return data[index];
   }
-  data = new T[size];
-  capacity = size;
-  this->size = size;
-}
-
-template <typename T>
-Array<T>::Array(std::initializer_list<T> list)
-    : size(list.size()), capacity(list.size()) {
-  data = new T[capacity];
-  std::copy(list.begin(), list.end(), data);
-}
-
-template <typename T>
-Array<T>::~Array() {
-  delete[] data;
-}
-
-// 添加元素
-template <typename T>
-void Array<T>::push(const T &value) {
-  expand();
-  data[size] = value;
-  size++;
-}
-
-// 删除指定下标的元素
-template <typename T>
-void Array<T>::remove(size_t index) {
-  if (index >= size || size == 0) {
-    throw out_of_range("[UmbralKeys Array::remove] Index out of range");
-  }
-
-  for (size_t i = index; i < size - 1; ++i) {
-    data[i] = data[i + 1];
-  }
-  --size;
-}
-
-// 清空所有元素
-template <typename T>
-void Array<T>::clear() {
-  size = 0;
-}
-
-template <typename T>
-size_t Array<T>::getSize() const {
-  return size;
-}
-// 获取当前数组的长度
-
-template <typename T>
-NativeArray<T> Array<T>::toNativeArray() const {
-  // 返回一个新的数组，包含当前的元素
-  T *arr = new T[size];
-  for (size_t i = 0; i < size; ++i) {
-    arr[i] = data[i];
-  }
-
-  NativeArray<T> nativeArray;
-  nativeArray.data = arr;
-  nativeArray.size = size;
-  return nativeArray;
-}
-
-// 获取指定索引的元素
-template <typename T>
-T &Array<T>::operator[](size_t index) {
-  if (index >= size || index < 0 || size == 0) {
-    throw out_of_range("[UmbralKeys Array::operator[]] Index out of range");
-  }
-  return data[index];
-}
-
-template <typename T>
-const T &Array<T>::operator[](size_t index) const {
-  if (index >= size || index < 0 || size == 0) {
-    throw out_of_range("[UmbralKeys Array::operator[]] Index out of range");
-  }
-  return data[index];
-}
+};
