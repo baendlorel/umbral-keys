6ea9b6cdf90caf6b670bf4fe9f39b12ebe9f0bd3
aldia_thinkpad <futami16237@gmail.com>
2025-04-10 00:11:46 +0800

    至此可以成功创建自定义窗口了

--- umbral-keys/resource.h
+++ umbral-keys/resource.h
@@ -2,14 +2,15 @@
 // Microsoft Visual C++ 生成的包含文件。
 // 供 umbral-keys.rc 使用
 //
-#define IDI_ICON1                       101
 #define IDI_APP_ICON                    101
+#define IDD_DIALOG1                     103
+#define IDD_CONFIG_EDITOR               103
 
 // Next default values for new objects
 // 
 #ifdef APSTUDIO_INVOKED
 #ifndef APSTUDIO_READONLY_SYMBOLS
-#define _APS_NEXT_RESOURCE_VALUE        103
+#define _APS_NEXT_RESOURCE_VALUE        105
 #define _APS_NEXT_COMMAND_VALUE         40001
 #define _APS_NEXT_CONTROL_VALUE         1001
 #define _APS_NEXT_SYMED_VALUE           101
